4b35aa685deb69c6a03faab86e5c50778f65b2e4
Mahima Mishra <mishramahima45586@gmail.com>
2026-06-09 18:26:49 +0530

    Added secure notes app

---
+++ wp3.py
@@ -0,0 +1,15 @@
+import pywhatkit as kit
+import datetime
+
+time = datetime.datetime.now()
+
+hour = time.hour
+minute = time.minute + 2
+
+kit.sendwhatmsg(
+    "+917488748611",
+    "Hello from ai",
+    11,
+    58
+    
+)
